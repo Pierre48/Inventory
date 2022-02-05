37088d06498e1eee4ea1a807e88f19fbef05c8d7
Pierre Plagnes <pierre.plagnes@gmail.com>
2022-02-05 10:20:18 +0100

    Onpress

--- src/inventory.app/Components/item-list.js
+++ src/inventory.app/Components/item-list.js
@@ -1,52 +1,63 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+import {Alert,LogBox, Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 
+const Item = (p) => {
+    return (
+        <View style={styles.center}>
+               <TouchableOpacity
+                    style={styles.container}>
+                    <Text style={styles.text} onPress={() =>console.log(p.item.barcode)}
+                        > {p.item.barcode} </Text>
+                </TouchableOpacity>
+        </View>
+    );
+}
 class List extends Component {
-   state = {
-      data: []
-   }
-   alertItemName = (item) => {
-      alert(item.name)
-   }
-   componentDidMount = () => {
-      fetch('https://localhost:7085/api/Item', {
-         method: 'GET',
+    state = {
+        data: []
+    }
+    componentDidMount = () => {
+        fetch('https://localhost:7085/api/Item', {
+            method: 'GET',
 
-      })
-      .then((response) => response.json())
-      .then((responseJson) => {
-         console.log(responseJson);
-         this.setState({
-            data: responseJson,
-         })
-      })
-      .catch((error) => {
-         console.error(error);
-      });
-   }
-   render() {
-      return (
-         <View>
-         { this.state.data.map((item)=>(
-            <TouchableOpacity
-            style = {styles.container}>
-               <Text style = {styles.text}> { item.barcode } </Text>
-            </TouchableOpacity>
-         ))}
-         </View>
-      )
-   }
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                this.setState({
+                    data: responseJson,
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+
+
+    render() {
+        return (
+            <View>
+                {this.state.data.map((item) => (
+                    <Item item={item}></Item>
+                ))}
+            </View>
+        )
+    }
 }
 export default List
 
-const styles = StyleSheet.create ({
-   container: {
-      padding: 10,
-      marginTop: 3,
-      backgroundColor: '#d9f9b1',
-      alignItems: 'center',
-   },
-   text: {
-      color: '#4f603c'
-   }
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        marginTop: 3,
+        backgroundColor: '#d9f9b1',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#4f603c'
+    },
+    center: {
+        alignItems: 'center'
+      }
 })
